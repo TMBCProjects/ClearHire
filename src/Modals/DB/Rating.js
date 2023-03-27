bd73cb5533d1b44f8aeb197bcf493a9b60a02eb8
srivz <srivathsananim@gmail.com>
2023-03-27 21:11:36 +0530

    modified:   src/Modals/DB/Employee.js 	new file:   src/Modals/DB/Offer.js 	new file:   src/Modals/DB/Rating.js

---
+++ src/Modals/DB/Rating.js
@@ -0,0 +1,24 @@
+export default class Rating {
+  isActive = false;
+  ratedByName = "";
+  ratedById = "";
+  ratedByEmail = "";
+  employeeName = "";
+  employeeId = "";
+  employeeEmail = "";
+  dateOfReview = "";
+  communication = "";
+  attitude = "";
+  abilityToLearn = "";
+  punctuality = "";
+  commitment = "";
+  trustworthiness = "";
+  skill = "";
+  teamPlayer = "";
+  note = "";
+  // designation = "";
+  // companyId = "";
+  // companyName = "";
+  // typeOfEmployment = "";
+  // salary = "";
+}
