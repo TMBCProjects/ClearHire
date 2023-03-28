f8ec0ae63d83487bbddc3bab770fd9e5ab52f5b4
giridhar <giribala2001@gmail.com>
2023-03-29 00:23:58 +0530

    major changes

--- src/components/Input/InputField.jsx
+++ src/components/Input/InputField.jsx
@@ -1,17 +1,59 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
+import eye from "../../../src/assets/images/eye.svg"
+import eyeStrike from "../../../src/assets/images/eyeStrike.svg"
 
 export default function InputField(props) {
     const [value, setValue] = useState("")
-    useEffect(()=>{
-        setValue(props.value)
-    }, [])
+    const [showPassword, setShowPassword] = useState(true);
 
-    onChange
+    function showPass() {
+        setShowPassword(true)
+        var x = document.getElementById("field");
+        if(x.type === "password") x.type = "text";
+    }
+
+    function hidePass() {
+        setShowPassword(false)
+        var x = document.getElementById("field");
+        if(x.type === "password") x.type = "text";
+    }
+
+    function onChange(event){
+        setValue(event.target.value)
+    }
   return (
       <div className="form-group">
           <label className="control-label">{props.label}</label>
           <div className="input">
-              <input type={props.type} className="form-control" name="email" value={value} placeholder={props.placeholder} />
+              <input type={props.type === "password" && showPassword? "password" : "text" } 
+              className="form-control" 
+              id="field"
+              name="email" 
+              value={value} 
+              placeholder={props.placeholder} 
+              onChange={onChange}/>
+              {props.type === "password" ?
+                    showPassword?
+                        <img 
+                            src = { eyeStrike } 
+                            alt = { 'show pass' } 
+                            width = { 20 } 
+                            className = "showPass" 
+                            onClick = { hidePass }>
+                        </img>
+                            :
+                        <img 
+                            src={eye} 
+                            alt={'show pass'} 
+                            width={20} 
+                            className="showPass" 
+                            onClick={showPass}>
+                        </img>
+
+                        :
+                        ""
+
+                }
           </div>
       </div>
   )
--- src/pages/Signup/SignupEmployer.jsx
+++ src/pages/Signup/SignupEmployer.jsx
@@ -1,21 +1,8 @@
 import React, { useState } from "react";
-import eye from "../../../src/assets/images/eye.svg"
-import eyeStrike from "../../../src/assets/images/eyeStrike.svg"
+import InputField from "../../components/Input/InputField";
 import "../Signup/Signup.css"
+
 export default function Signup() {
-    const [showPassword, setShowPassword] = useState(true);
-
-    function showPass() {
-        setShowPassword(true)
-        var x = document.getElementById("password-field");
-        x.type = "password";
-    }
-
-    function hidePass() {
-        setShowPassword(false)
-        var x = document.getElementById("password-field");
-        x.type = "text";
-    }
     return (
         <div className="container-fluid">
             <div className="signupHeader">
@@ -23,24 +10,17 @@ export default function Signup() {
             </div>
             <form className="form-horizontal">
 
-                <div className="form-group">
-                    <label className="control-label">Email</label>
-                    <div className="input">
-                        <input type="email" className="form-control" name="email" value=""  placeholder="Input your company mail id, use an official email address."/>
-                    </div>
-                </div>
+                <InputField 
+                label={"Email"} 
+                type={"email"} 
+                placeholder={"Input your company mail id, use an official email address."}
+                />
 
-                <div className="form-group">
-                    <label className="control-label">Password</label>
-                    <div className="input">
-                        <input id="password-field" type="password" className="form-control" name="password" value="secret"   placeholder="Input your password in here."/>
-                        {showPassword ?
-                            <img src={eyeStrike} alt={'show pass'} width={20} className="showPass" onClick={hidePass}></img>
-                            :
-                            <img src={eye} alt={'show pass'} width={20} className="showPass" onClick={showPass}></img>
-                        }
-                    </div>
-                </div>
+                <InputField
+                    label={"Password"}
+                    type={"password"}
+                    placeholder={"Input your password in here."}
+                />
 
                 <div className="form-group">
                     <label className="control-label">Company Name</label>
