8ac72c10f401373d833c492fec1a4ddc3d388a8f
giridhar <giribala2001@gmail.com>
2023-03-28 11:18:40 +0530

    project structure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "../../components/About/About";
+import Contact from "../../components/Contact/Contact";
+import Banner from "../../components/Bannner/Banner";
+
+const Home = () => {
+  return <div>
+        <Banner />
+        <About />
+        <Contact />
+  </div>;
+};
+
+export default Home;
