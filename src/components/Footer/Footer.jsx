9db70a7d94fc1969e159b486ff7b12cafedc9ebb
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-03-28 19:01:17 +0530

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,30 +6,30 @@ const Footer = () => {
       <div className="container" id="footer">
         <div className="row d-flex justify-content-center align-items-center py-3">
           <div className="col-md-8 col-sm-12">
-            <a className="mx-3 text-decoration-none text-color-light" href="/">
+            <a className="mx-3 text-decoration-none text-gray" href="/">
               About
             </a>
-            <a className="mx-3 text-decoration-none text-color-light" href="/">
+            <a className="mx-3 text-decoration-none text-gray" href="/">
               Careers
             </a>
-            <a className="mx-3 text-decoration-none text-color-light" href="/">
+            <a className="mx-3 text-decoration-none text-gray" href="/">
               Privacy
             </a>
-            <a className="mx-3 text-decoration-none text-color-light" href="/">
+            <a className="mx-3 text-decoration-none text-gray" href="/">
               Cookies
             </a>
-            <a className="mx-3 text-decoration-none text-color-light" href="/">
+            <a className="mx-3 text-decoration-none text-gray" href="/">
               Terms
             </a>
-            <a className="mx-3 text-decoration-none text-color-light" href="/">
+            <a className="mx-3 text-decoration-none text-gray" href="/">
               Help
             </a>
-            <a className="mx-3 text-decoration-none text-color-light" href="/">
+            <a className="mx-3 text-decoration-none text-gray" href="/">
               Feedback
             </a>
           </div>
           <div className="col-md-2">
-            <p className="text-color-light my-0">Copyright &copy; clearhire</p>
+            <p className="text-gray my-0">Copyright &copy; clearhire</p>
           </div>
         </div>
       </div>
