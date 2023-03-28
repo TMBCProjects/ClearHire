c2861c62ccc38aa890bafb06185913914b212899
srivz <srivathsananim@gmail.com>
2023-03-28 21:05:24 +0530

    modified:   src/pages/Home/Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Login from "../../components/Login/Login";
+import About from "../../components/About/About";
+import Banner from "../../components/Bannner/Banner";
 
 const Home = () => {
     return (
