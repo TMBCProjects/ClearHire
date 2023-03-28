933a512d5c1cf3933ea8ba2fb1aa7180a1ac09dc
giridhar <giribala2001@gmail.com>
2023-03-29 00:01:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Signup from "./pages/Signup/Signup";
+import SignupEmployer from './pages/Signup/SignupEmployer';
+import Signup_Options from './pages/Signup_Options';
+import Signup_Choose_User from './pages/Signup_Choose_User';
+import Signup_Done from './pages/Signup_Done';
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signupEmployer" element={<SignupEmployer />}></Route>
+          <Route path="/signup-options" element={<Signup_Options />}></Route>
+          <Route path="/user-options" element={<Signup_Choose_User />}></Route>
+          <Route path="/signup-done" element={<Signup_Done />}></Route>
         </Routes>
         <div className="footer shadow shadow-sm bg-light">
           <Footer />
