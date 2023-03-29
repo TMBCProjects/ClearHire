d23d0e65ffc1750975ab4129de7f64baa801b374
srivz <srivathsananim@gmail.com>
2023-03-29 13:12:13 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,31 @@ import Home from './pages/Home/Home';
 import Footer from "./components/Footer/Footer";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import SignupEmployer from './pages/Signup/SignupForm/SignupEmployer';
-import Signup_Options from './pages/Signup/Signup_Options';
-import Signup_Choose_User from './pages/Signup/Signup_Choose_User';
-import Signup_Done from './pages/Signup/Signup_Done';
+import SignupOptions from "./pages/Signup/SignupOptions";
+import SignupChooseUser from "./pages/Signup/SignupChooseUser";
+import SignupDone from "./pages/Signup/SignupDone";
 
 function App() {
-  return(
+  return (
     <div>
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signupEmployer" element={<SignupEmployer />}></Route>
-          <Route path="/signup-options" element={<Signup_Options />}></Route>
-          <Route path="/user-options" element={<Signup_Choose_User />}></Route>
-          <Route path="/signup-done" element={<Signup_Done />}></Route>
+          <Route
+            path="/"
+            element={<Home />}></Route>
+          <Route
+            path="/signupEmployer"
+            element={<SignupEmployer />}></Route>
+          <Route
+            path="/signup-options"
+            element={<SignupOptions />}></Route>
+          <Route
+            path="/user-options"
+            element={<SignupChooseUser />}></Route>
+          <Route
+            path="/signup-done"
+            element={<SignupDone />}></Route>
         </Routes>
         <div className="footer shadow shadow-sm bg-light">
           <Footer />
