afccb7b44842dbc29e3327f9a770aa8a40b91b8d
Berdin jasper <berdin1.jasper@gmail.com>
2023-03-30 01:00:19 -0400

    footer changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-white">
+    <footer className="bg-white footer">
       <div className="container my-3" id="footer">
         <div className="row d-flex justify-content-center align-items-center py-3 foot-con">
           <div className="col-md-8 col-12 foot-item">
-            <a className=" mx-3 text-decoration-none text-color-light" href="/">About</a>
-            <a className="mx-3 text-decoration-none text-color-light" href="/">
+            <a className=" mx-3 text-decoration-none text-gray" href="/">About</a>
+            <a className="mx-3 text-decoration-none text-gray" href="/">
               Careers
             </a>
             <a className="mx-3 text-decoration-none text-gray" href="/">
@@ -28,7 +28,7 @@ const Footer = () => {
             </a>
           </div>
           <div className="col-md-2">
-            <p className="text-color-light px-3 my-0 copy">Copyright &copy; clearhire</p>
+            <p className=" px-3 my-0 text-gray">Copyright &copy; clearhire</p>
           </div>
         </div>
       </div>
