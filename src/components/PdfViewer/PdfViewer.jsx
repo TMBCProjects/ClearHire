81fc9e6902ed07d1dcbd6d11e16a3847d0e07c2f
srivz <srivathsananim@gmail.com>
2023-04-03 15:50:13 +0530

    modified:   package-lock.json 	modified:   package.json 	renamed:    src/components/PdfViewer/PdfViewer.js -> src/components/PdfViewer/PdfViewer.jsx 	modified:   src/pages/Employee/EmployeeOfferLetter/EmployeeOfferLetter.jsx

--- src/components/PdfViewer/PdfViewer.jsx
+++ src/components/PdfViewer/PdfViewer.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
+import React, { useEffect, useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-function PdfViewer({ pdfUrl }) {
+const PdfViewer = ({ pdfUrl }) => {
+  const [file, setFile] = useState('');
   const [numPages, setNumPages] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
 
+  useEffect(() => {
+    setFile('' + pdfUrl)
+  }, [pdfUrl])
   const onNextPage = () => {
     setCurrentPage(Math.min(currentPage + 1, numPages));
   };
@@ -26,26 +31,33 @@ function PdfViewer({ pdfUrl }) {
           alignItems: "center",
           justifyContent: "center",
         }}>
-        <button
-          disabled={currentPage === 1}
-          onClick={onPrevPage}>
-          &lt;
-        </button>
         <div style={{ width: "500px" }}>
           <Document
-            file={pdfUrl}
+            file={file}
             onLoadSuccess={onDocumentLoadSuccess}>
             <Page pageNumber={currentPage} />
           </Document>
         </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+        <button
+          disabled={currentPage === 1}
+          onClick={onPrevPage}>
+          Prev
+        </button>
+        <div style={{ textAlign: "center", marginTop: "10px" }}>
+          {currentPage} / {numPages}
+        </div>
         <button
           disabled={currentPage === numPages}
           onClick={onNextPage}>
-          &gt;
+          Next
         </button>
-      </div>
-      <div style={{ textAlign: "center", marginTop: "10px" }}>
-        Page {currentPage} of {numPages}
       </div>
     </div>
   );
--- src/pages/Employee/EmployeeOfferLetter/EmployeeOfferLetter.jsx
+++ src/pages/Employee/EmployeeOfferLetter/EmployeeOfferLetter.jsx
@@ -4,32 +4,6 @@ import PdfViewer from "../../../components/PdfViewer/PdfViewer";
 
 const EmployeeOfferLetter = () => {
     let pdfUrl = "https://firebasestorage.googleapis.com/v0/b/clearhire-d91d9.appspot.com/o/profileImages%2FTeambo%20changes%20(1).pdf?alt=media&token=8d8d7c79-683f-4156-8815-e5f1dec5e5d9";
-    const [currentPage, setCurrentPage] = useState(1);
-    let totalPages = 2;
-    const iframeRef = useRef(null);
-
-
-    useEffect(() => {
-        const iframe = iframeRef.current;
-        const url = `${pdfUrl}#toolbar=0&page=${currentPage}`;
-        // iframe.onload = () => {
-        //     const win = iframe.contentWindow || iframe.contentDocument.defaultView;
-        //     win.scrollTo(0, 0);
-        // };
-        iframe.src = url;
-    }, [currentPage, pdfUrl]);
-
-    const prevPage = async () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1)
-        }
-    };
-
-    const nextPage = async () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1)
-        }
-    };
 
     return (
         <div className="container" id="offerletter">
@@ -85,7 +59,9 @@ const EmployeeOfferLetter = () => {
                 </div>
                 <div className="col-md-8">
                     <div className="offerletter-pdf">
-                        <PdfViewer pdfUrl={pdfUrl} />
+                        {/* <iframe className="iframe-no-scroll" src={`${pdfUrl}#toolbar=0&scrollbar=0&page=2`} /> */}
+
+                        <PdfViewer pdfUrl={`${pdfUrl}`} />
                     </div>
                 </div>
             </div>
