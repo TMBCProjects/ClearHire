28af3a015998049ec23aa29f70039f280506a36d
Basu-Dev-Ghosh <basu1735@gmail.com>
2023-04-09 12:14:38 +0530

    added remove button

--- src/pages/Employee/Profile/Profile.jsx
+++ src/pages/Employee/Profile/Profile.jsx
@@ -6,6 +6,7 @@ import Check from "../../../assets/images/Check.svg";
 import InputField from '../../../components/Input/InputField';
 import { Slider, Col } from 'antd';
 import UploadFile from '../../../components/UploadFile';
+import { MinusOutlined } from '@ant-design/icons';
 
 const initialValues = {
     link: "",
@@ -14,37 +15,45 @@ const initialValues = {
 }
 export default function Profile() {
     const [values, setValues] = useState(initialValues)
-    const [skills,setSkills]=useState([
+    const [skills, setSkills] = useState([
         {
-            skillName:"",
-            value:0
+            skillName: "",
+            value: 0
         },
         {
-            skillName:"",
-            value:0 
+            skillName: "",
+            value: 0
         }
     ])
     const [inputValue, setInputValue] = useState(1);
     const onChange = (newValue) => {
         setInputValue(newValue);
     };
-    const handleInputChange = (e,index) => {
+    const handleInputChange = (e, index) => {
         const { name, value } = e.target;
         // setValues({
         //     ...values,
         //     [name]: value,
         // });
-        const newState = skills.map((obj,id) => {
+        const newState = skills.map((obj, id) => {
             // 👇️ if id equals 2, update country property
             if (id === index) {
-              return {...obj, skillName: value};
+                return { ...obj, skillName: value };
             }
-      
+
             // 👇️ otherwise return the object as is
             return obj;
-          });
-      
-          setSkills(newState);
+        });
+
+        setSkills(newState);
+    };
+
+    const removeSkill = (index) => {
+        const newState = skills.filter((obj, id) => {
+            return id !== index
+        });
+
+        setSkills(newState);
     };
     return (
         <div className='profile'>
@@ -61,7 +70,7 @@ export default function Profile() {
             </div>
 
             <div className='profileBody'>
-            <UploadFile/>
+                <UploadFile />
                 <InputField
                     type={"text"}
                     name={"link"}
@@ -80,55 +89,75 @@ export default function Profile() {
 
                 <div className='skills'>
                     <span style={{ fontWeight: "bold", display: "flex", gap: "2vh", alignItems: "center" }}>Your Skills
-                       </span>
-                       {
-                        skills?.map((skill,index)=>{
-                            return(
-                             <div className='skillList'>
-                                <InputField
-                                    type={"text"}
-                                    name={`skill ${index+1}`}//name should iterated according to .map
-                                    onChange={(e)=>{
-                                        handleInputChange(e,index)
-                                    }}
-                                    placeholder={`Skill ${index+1}`}
-                                />
-                                <Col span={10}>
-                                    <Slider
-                                        min={1}
-                                        max={100}
-                                        // onChange={onChange}
-                                        // value={typeof inputValue === 'number' ? inputValue : 0}
-                                        trackStyle={{ backgroundColor: "#00823B" }}
-                                        handleStyle={{ backgroundColor: "#00823B" }}
+                    </span>
+                    {
+                        !skills.length && <div>Add atleast 1 skill <span onClick={() => {
+                            setSkills([...skills, {
+                                skillName: "",
+                                value: 0
+                            }])
+                        }} style={{ border: "1px solid green", borderRadius: "6vh", marginLeft: "2vh", padding: "1vh 1.5vh", width: "7vh", cursor: "pointer" }}>
+                            <img src={Add} alt="add"></img>
+                        </span></div>
+                    }
+                    {
+                        skills?.map((skill, index) => {
+                            return (
+
+
+                                <div className='skillList'>
+
+                                    <InputField
+                                        type={"text"}
+                                        name={`skill ${index + 1}`}//name should iterated according to .map
+                                        onChange={(e) => {
+                                            handleInputChange(e, index)
+                                        }}
+                                        placeholder={`Skill ${index + 1}`}
                                     />
-                                </Col>
-        
-                                <span className='sliderpercent'>{inputValue}</span>
-                                {
-                                    index===skills.length-1 && 
-                                <span onClick={()=>{
-                                    setSkills([...skills,{
-                                        skillName:"",
-                                        value:0
-                                    }])
-                                }} style={{ border: "1px solid green", borderRadius: "6vh",marginLeft: "2vh", padding: "1vh 1.5vh", width: "7vh", cursor: "pointer" }}>
-                                    <img src={Add} alt="add"></img>
-                                </span>
-                                }
-                            </div>
+                                    <Col span={10}>
+                                        <Slider
+                                            min={1}
+                                            max={100}
+                                            // onChange={onChange}
+                                            // value={typeof inputValue === 'number' ? inputValue : 0}
+                                            trackStyle={{ backgroundColor: "#00823B" }}
+                                            handleStyle={{ backgroundColor: "#00823B" }}
+                                        />
+                                    </Col>
+
+                                    <span className='sliderpercent'>{inputValue}</span>
+
+                                    <span onClick={() => {
+                                        removeSkill(index)
+                                    }} style={{ border: "1px solid tomato", borderRadius: "6vh", marginLeft: "2vh", padding: "1vh 1.5vh", width: "7vh", cursor: "pointer", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                        <MinusOutlined style={{ color: "tomato" }} />
+                                    </span>
+                                    {
+                                        index === skills.length - 1 &&
+                                        <span onClick={() => {
+                                            setSkills([...skills, {
+                                                skillName: "",
+                                                value: 0
+                                            }])
+                                        }} style={{ border: "1px solid green", borderRadius: "6vh", marginLeft: "2vh", padding: "1vh 1.5vh", width: "7vh", cursor: "pointer" }}>
+                                            <img src={Add} alt="add"></img>
+                                        </span>
+                                    }
+                                </div>
+
                             )
                         })
-                       }
-                       
-                   
+                    }
 
-                
-                    
+
+
+
+
                     <div className='profileFooter'>
                         <button><img src={Check} alt="submit-logo" ></img>&nbsp;Done</button>
                     </div>
-                </div>
+                </div >
             </div>
         </div>
     )
