48679e6e36d367de118928ada7bc6ded1d32fe90
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-04-11 12:55:56 +0530

    employer api

--- src/components/Cards/AssesmentCard.js
+++ src/components/Cards/AssesmentCard.js
@@ -1,39 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 //import image from "../../assets/images/Image.png"
-import pic from "../../assets/images/pic.png"
-import ProgressBar from '../ProgressBar'
-import './styles.css'
-const Assesment_Card = ({value}) => {
-    return (
-        <div className='vreqcard assess-card'>
+import pic from "../../assets/images/pic.png";
+import ProgressBar from "../ProgressBar";
+import "./styles.css";
+import { readEmployerDetails } from "../../DataBase/Employer/employer";
+const Assesment_Card = ({ value }) => {
+  const [employer, seEmployer] = useState([]);
+  useEffect(() => {
+    const fetchEmployerDetails = async () => {
+      const data = await readEmployerDetails();
+      console.log(data);
+    };
+    fetchEmployerDetails();
+  });
+  return (
+    <div className="vreqcard assess-card">
+      <div className="managerlogo">
+        <img src={pic} alt="manager-logo"></img>
+        <ProgressBar value={value} />
+      </div>
+      <div className="headDesc">
+        <span>Govarthini</span>
+        <span>Chennai, India</span>
+        <span
+          style={{
+            background: "#D7F2BC 0% 0% no-repeat padding-box",
+            borderRadius: "9px",
+            width: "50%",
+            marginTop: ".5rem",
+            padding: ".5rem",
+            textAlign: "center",
+            color: "#66BC11",
+            fontWeight: "bold",
+          }}
+        >
+          Project Manager
+        </span>
+      </div>
 
-            <div className='managerlogo'>
-                <img src={pic} alt="manager-logo"></img>
-                <ProgressBar value={value}/>
-            </div>
-            <div className='headDesc'>
-                <span>Govarthini</span>
-                <span>Chennai, India</span>
-                <span style={{
-                    background: "#D7F2BC 0% 0% no-repeat padding-box",
-                    borderRadius: "9px",
-                    width:"50%",
-                    marginTop:".5rem",
-                    padding:".5rem",
-                    textAlign:"center",
-                    color:"#66BC11",
-                    fontWeight:"bold"
-                }}>Project Manager</span>
-            </div>
+      <div className="cardFooter">
+        <button className="allow">Assess Employee</button>
+      </div>
+    </div>
+  );
+};
 
-
-            <div className='cardFooter'>
-                <button className='allow'>Assess Employee</button>
-            </div>
-
-
-        </div>
-    )
-}
-
-export default Assesment_Card
+export default Assesment_Card;
