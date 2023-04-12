aeec13d53c581414f97b0691b3ed83301c69fcfe
giridhar <giribala2001@gmail.com>
2023-04-12 11:52:33 +0530

    vreq

--- src/pages/Employer/VerficationRequest/VerficationRequest.jsx
+++ src/pages/Employer/VerficationRequest/VerficationRequest.jsx
@@ -27,14 +27,23 @@ export default function VerficationRequest() {
             </div>
           </div>
 
-          <div className='cardBody'>
-            <div className='managerlogo'>
-              <img src={pic} alt="manager-logo"></img>
+          <div className='cardFooter'>
+            <button className='allow'>Allow Access&nbsp;<img src={check} alt="check"></img></button>
+            <button className='deny'>Deny&nbsp;<img src={close} alt="close"></img></button>
+          </div>
+
+
+        </div>
+
+        <div className='vreqcard'>
+
+          <div className='vreqcardHead'>
+            <div className='companylogo'>
+              <img src={image} alt="company-logo"></img>
             </div>
             <div className='headDesc'>
-              <span>Govarthini, 24</span><br />
-              <span>Project Manager</span><br />
-              <span>Chennai, India</span><br />
+              <span style={{ fontWeight: "bold" }}>The Madras Branding Company </span><br />
+              <span> wants to view your employee's assessments and details</span>
             </div>
           </div>
 
@@ -58,17 +67,6 @@ export default function VerficationRequest() {
             </div>
           </div>
 
-          <div className='cardBody'>
-            <div className='managerlogo'>
-              <img src={pic} alt="manager-logo"></img>
-            </div>
-            <div className='headDesc'>
-              <span>Govarthini, 24</span><br />
-              <span>Project Manager</span><br />
-              <span>Chennai, India</span><br />
-            </div>
-          </div>
-
           <div className='cardFooter'>
             <button className='allow'>Allow Access&nbsp;<img src={check} alt="check"></img></button>
             <button className='deny'>Deny&nbsp;<img src={close} alt="close"></img></button>
@@ -89,17 +87,6 @@ export default function VerficationRequest() {
             </div>
           </div>
 
-          <div className='cardBody'>
-            <div className='managerlogo'>
-              <img src={pic} alt="manager-logo"></img>
-            </div>
-            <div className='headDesc'>
-              <span>Govarthini, 24</span><br />
-              <span>Project Manager</span><br />
-              <span>Chennai, India</span><br />
-            </div>
-          </div>
-
           <div className='cardFooter'>
             <button className='allow'>Allow Access&nbsp;<img src={check} alt="check"></img></button>
             <button className='deny'>Deny&nbsp;<img src={close} alt="close"></img></button>
@@ -117,48 +104,6 @@ export default function VerficationRequest() {
             <div className='headDesc'>
               <span style={{ fontWeight: "bold" }}>The Madras Branding Company </span><br />
               <span> wants to view your employee's assessments and details</span>
-            </div>
-          </div>
-
-          <div className='cardBody'>
-            <div className='managerlogo'>
-              <img src={pic} alt="manager-logo"></img>
-            </div>
-            <div className='headDesc'>
-              <span>Govarthini, 24</span><br />
-              <span>Project Manager</span><br />
-              <span>Chennai, India</span><br />
-            </div>
-          </div>
-
-          <div className='cardFooter'>
-            <button className='allow'>Allow Access&nbsp;<img src={check} alt="check"></img></button>
-            <button className='deny'>Deny&nbsp;<img src={close} alt="close"></img></button>
-          </div>
-
-
-        </div>
-
-        <div className='vreqcard'>
-
-          <div className='vreqcardHead'>
-            <div className='companylogo'>
-              <img src={image} alt="company-logo"></img>
-            </div>
-            <div className='headDesc'>
-              <span style={{ fontWeight: "bold" }}>The Madras Branding Company </span><br />
-              <span> wants to view your employee's assessments and details</span>
-            </div>
-          </div>
-
-          <div className='cardBody'>
-            <div className='managerlogo'>
-              <img src={pic} alt="manager-logo"></img>
-            </div>
-            <div className='headDesc'>
-              <span>Govarthini, 24</span><br />
-              <span>Project Manager</span><br />
-              <span>Chennai, India</span><br />
             </div>
           </div>
 
