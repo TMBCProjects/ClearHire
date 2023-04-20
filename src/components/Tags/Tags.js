91f87e64fb835d0b8cc793c3262455120ea7e8c2
giridhar <giribala2001@gmail.com>
2023-04-21 00:49:44 +0530

    location

---
+++ src/components/Tags/Tags.js
@@ -0,0 +1,131 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Input, Space, Tag, Tooltip, theme } from 'antd';
+import { useEffect, useRef, useState } from 'react';
+const App = () => {
+  const { token } = theme.useToken();
+  const [tags, setTags] = useState([]);
+  const [inputVisible, setInputVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [editInputIndex, setEditInputIndex] = useState(-2);
+  const [editInputValue, setEditInputValue] = useState('');
+  const inputRef = useRef(null);
+  const editInputRef = useRef(null);
+  useEffect(() => {
+    if (inputVisible) {
+      inputRef.current?.focus();
+    }
+  }, [inputVisible]);
+  useEffect(() => {
+    editInputRef.current?.focus();
+  }, [inputValue]);
+  const handleClose = (removedTag) => {
+    const newTags = tags.filter((tag) => tag !== removedTag);
+    console.log(newTags);
+    setTags(newTags);
+  };
+  const showInput = () => {
+    setInputVisible(true);
+  };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleInputConfirm = () => {
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      setTags([...tags, inputValue]);
+    }
+    setInputVisible(false);
+    setInputValue('');
+  };
+  const handleEditInputChange = (e) => {
+    setEditInputValue(e.target.value);
+  };
+  const handleEditInputConfirm = () => {
+    const newTags = [...tags];
+    newTags[editInputIndex] = editInputValue;
+    setTags(newTags);
+    setEditInputIndex(-1);
+    setInputValue('');
+  };
+  const tagInputStyle = {
+    verticalAlign: 'top',
+    paddingBottom: "2vh"
+  };
+  const tagPlusStyle = {
+    background: token.colorBgContainer,
+    borderStyle: '1px solid #00823B ',
+    padding: "2vh",
+    display: "flex",
+    alignItems: "center",
+    gap:"1vh"
+  };
+  return (
+    <Space size={[0, 8]} wrap style={{display: "block", paddingBottom: "3vh"}}>
+      <Space size={[0, 8]} wrap>
+        {tags.map((tag, index) => {
+          if (editInputIndex === index) {
+            return (
+              <Input
+                ref={editInputRef}
+                key={tag}
+                size="small"
+                style={tagInputStyle}
+                value={editInputValue}
+                onChange={handleEditInputChange}
+                onBlur={handleEditInputConfirm}
+                onPressEnter={handleEditInputConfirm}
+              />
+            );
+          }
+          const isLongTag = tag.length > 20;
+          const tagElem = (
+            <Tag
+              key={tag}
+              closable={index !== -1}
+              style={{
+                userSelect: 'none',
+              }}
+              onClose={() => handleClose(tag)}
+            >
+              <span
+                style={{padding: "1vh", fontSize: "larger"}}
+                onDoubleClick={(e) => {
+                  if (index !== 0) {
+                    setEditInputIndex(index);
+                    setEditInputValue(tag);
+                    e.preventDefault();
+                  }
+                }}
+              >
+                {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+              </span>
+            </Tag>
+          );
+          return isLongTag ? (
+            <Tooltip title={tag} key={tag}>
+              {tagElem}
+            </Tooltip>
+          ) : (
+            tagElem
+          );
+        })}
+      </Space>
+      {inputVisible ? (
+        <Input
+          ref={inputRef}
+          type="text"
+          size="small"
+          style={tagInputStyle}
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleInputConfirm}
+          onPressEnter={handleInputConfirm}
+        />
+      ) : (
+        <Tag style={tagPlusStyle} onClick={showInput}>
+          <PlusOutlined /> Add Location
+        </Tag>
+      )}
+    </Space>
+  );
+};
+export default App;
