faa3d116f032d7046c43eec9a30fff46292ccd75
giridhar2000 <48080862+giridhar2000@users.noreply.github.com>
2023-04-25 22:22:02 +0530

    Update Signup.jsx

--- src/pages/Signup/SignupForm/Signup.jsx
+++ src/pages/Signup/SignupForm/Signup.jsx
@@ -1,6 +1,6 @@
 import { Button } from "antd";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Dropdown from "../../../components/Dropdrowns/Dropdown";
 import InputField from "../../../components/Input/InputField";
 import UploadPic from "../../../components/UploadPic/UploadPic";
@@ -8,6 +8,9 @@ import { registerLogin } from "../../../DataBase/SignUp/signUp";
 import "../SignupForm/Signup.css";
 import Loader from '../../../components/Loader'
 import Tags from "../../../components/Tags/Tags";
+import { PlusOutlined } from '@ant-design/icons';
+import { Input, Space, Tag, Tooltip, theme } from 'antd';
+
 const initialValues = {
   email: "",
   password: "",
@@ -41,12 +44,69 @@ export default function Signup() {
       });
   }, []);
 
+  const { token } = theme.useToken();
+  const [tags, setTags] = useState([]);
+  const [inputVisible, setInputVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [editInputIndex, setEditInputIndex] = useState(-2);
+  const [editInputValue, setEditInputValue] = useState('');
+  const inputRef = useRef(null);
+  const editInputRef = useRef(null);
+  useEffect(() => {
+    if (inputVisible) {
+      inputRef.current?.focus();
+    }
+  }, [inputVisible]);
+  useEffect(() => {
+    editInputRef.current?.focus();
+  }, [inputValue]);
+  const handleClose = (removedTag) => {
+    const newTags = tags.filter((tag) => tag !== removedTag);
+    console.log(newTags);
+    setTags(newTags);
+  };
+  const showInput = () => {
+    setInputVisible(true);
+  };
+
+  const handleInputConfirm = () => {
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      setTags([...tags, inputValue]);
+    }
+    setInputVisible(false);
+    setInputValue('');
+  };
+  const handleEditInputChange = (e) => {
+    setEditInputValue(e.target.value);
+  };
+  const handleEditInputConfirm = () => {
+    const newTags = [...tags];
+    newTags[editInputIndex] = editInputValue;
+    setTags(newTags);
+    setEditInputIndex(-1);
+    setInputValue('');
+  };
+  const tagInputStyle = {
+    verticalAlign: 'top',
+    paddingBottom: "2vh"
+  };
+  const tagPlusStyle = {
+    background: token.colorBgContainer,
+    borderStyle: '1px solid #00823B ',
+    padding: "2vh",
+    display: "flex",
+    alignItems: "center",
+    gap:"1vh"
+  };
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setValues({
       ...values,
       [name]: value,
     });
+    setInputValue(e.target.value);
+
   };
 
   const handleSubmit = () => {
@@ -180,7 +240,73 @@ export default function Signup() {
           {user === "Employer" ? (
             <>
             <label className="control-label">Company Location</label>
-              <Tags />
+            <Space size={[0, 8]} wrap style={{display: "block", paddingBottom: "3vh"}}>
+      <Space size={[0, 8]} wrap>
+        {tags.map((tag, index) => {
+          if (editInputIndex === index) {
+            return (
+              <Input
+                ref={editInputRef}
+                key={tag}
+                size="small"
+                style={tagInputStyle}
+                value={editInputValue}
+                onChange={handleEditInputChange}
+                onBlur={handleEditInputConfirm}
+                onPressEnter={handleEditInputConfirm}
+              />
+            );
+          }
+          const isLongTag = tag.length > 20;
+          const tagElem = (
+            <Tag
+              key={tag}
+              closable={index !== -1}
+              style={{
+                userSelect: 'none',
+              }}
+              onClose={() => handleClose(tag)}
+            >
+              <span
+                style={{padding: "1vh", fontSize: "larger"}}
+                onDoubleClick={(e) => {
+                  if (index !== 0) {
+                    setEditInputIndex(index);
+                    setEditInputValue(tag);
+                    e.preventDefault();
+                  }
+                }}
+              >
+                {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+              </span>
+            </Tag>
+          );
+          return isLongTag ? (
+            <Tooltip title={tag} key={tag}>
+              {tagElem}
+            </Tooltip>
+          ) : (
+            tagElem
+          );
+        })}
+      </Space>
+      {inputVisible ? (
+        <Input
+          ref={inputRef}
+          type="text"
+          size="small"
+          style={tagInputStyle}
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleInputConfirm}
+          onPressEnter={handleInputConfirm}
+        />
+      ) : (
+        <Tag style={tagPlusStyle} onClick={showInput}>
+          <PlusOutlined /> Add Location
+        </Tag>
+      )}
+    </Space>
             </>
           ) : (
               <></>
