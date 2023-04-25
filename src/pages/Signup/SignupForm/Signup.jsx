56f9bc35c188ccf16c92806da5579335a5c26d48
srivz <srivathsananim@gmail.com>
2023-04-25 22:32:08 +0530

    modified:   src/pages/Signup/SignupForm/Signup.jsx

--- src/pages/Signup/SignupForm/Signup.jsx
+++ src/pages/Signup/SignupForm/Signup.jsx
@@ -1,5 +1,4 @@
 import { Button } from "antd";
-import axios from "axios";
 import React, { useState, useEffect, useRef } from "react";
 import Dropdown from "../../../components/Dropdrowns/Dropdown";
 import InputField from "../../../components/Input/InputField";
@@ -7,7 +6,6 @@ import UploadPic from "../../../components/UploadPic/UploadPic";
 import { registerLogin } from "../../../DataBase/SignUp/signUp";
 import "../SignupForm/Signup.css";
 import Loader from '../../../components/Loader'
-import Tags from "../../../components/Tags/Tags";
 import { PlusOutlined } from '@ant-design/icons';
 import { Input, Space, Tag, Tooltip, theme } from 'antd';
 
@@ -15,15 +13,12 @@ const initialValues = {
   email: "",
   password: "",
   name: "",
-  state: "",
-  country: "",
   profileImage: "",
   role: "",
 };
 
 export default function Signup() {
   const [values, setValues] = useState(initialValues);
-  //  const [countries, setCountries] = useState([""]);
   const [loading, setLoading] = useState(false)
   let year = Array.from(
     { length: 123 },
@@ -33,13 +28,6 @@ export default function Signup() {
   let month = Array.from({ length: 12 }, (_, i) => i + 1);
   var selectedYear = "";
   var user = sessionStorage.getItem("user");
-  // useEffect(() => {
-  //   fetch("https://restcountries.com/v2/all?fields=name")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setCountries(data);
-  //     });
-  // }, []);
 
   const { token } = theme.useToken();
   const [tags, setTags] = useState([]);
@@ -110,7 +98,8 @@ export default function Signup() {
     values.profileImage = sessionStorage.getItem("profileImage");
     values.role = user;
     if (user === "Employer") {
-      values.companyLocations = sessionStorage.getItem("locations");
+      values.companyLocations = tags;
+      console.log(values);
     }
     setLoading(true);
     registerLogin(values).then(() => {
