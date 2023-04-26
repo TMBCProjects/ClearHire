50eafc1f8d6249c1329b2d850face9f001c45093
giridhar2000 <48080862+giridhar2000@users.noreply.github.com>
2023-04-26 14:27:58 +0530

    Update EmployeeAssesmentForm.jsx

--- src/pages/EmployeeAssessment/EmployeeAssesmentForm.jsx
+++ src/pages/EmployeeAssessment/EmployeeAssesmentForm.jsx
@@ -78,61 +78,6 @@ export default function EmployeeAssesmentForm() {
 
         </div>
       </div>
-
-
-
-
-      <div className="form">
-        <form action="" className='assessform-1'>
-
-          <div className="form-1">
-          <input type="text" placeholder='Untitled review form' className='f-1' />
-          <input type="text" placeholder='Description' className='f-2' />
-          </div>
-
-          <div className="form-2">
-            <label htmlFor="">Q1</label>
-            <input type="text" placeholder='Enter Question' className='f-3' />
-          </div>
-
-          <div className="form-3">
-            <label htmlFor="">Answer type</label>
-            <input type="text" placeholder='Select' className='f-4' />
-          </div>
-          <hr />
-          <div className="form-2">
-            <label htmlFor="">Q2</label>
-            <input type="text" placeholder='Enter Question' className='f-3' />
-          </div>
-           
-          <div className="form-4">
-            <label htmlFor="">Answer type</label>
-            <input type="text" placeholder='Multiple choice' className='f-4' />
-          </div>
-          <div>
-            <input type="checkbox"/>
-            <label for="vehicle1"> Choice 01</label>
-            <br />
-            <input type="checkbox"/>
-            <label for="vehicle2"> Choice 02</label><br></br>
-          </div>
-          <hr />
-          <div className="form-2">
-            <label htmlFor="">Q3</label>
-            <input type="text" placeholder='Enter Question' className='f-3' />
-          </div>
-
-          <div className="form-3">
-            <textarea type="text" placeholder='Paragraph' rows="1" cols="50" className='f-4' />
-          </div>
-        </form>
-        <div className="add">
-            <p></p>
-            {/* <img src={add} alt="" />   */}
-            <button className='add-btn'>Answer question</button>
-
-        </div>
-      </div>
     </div>
   </div>
   
