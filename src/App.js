737b2f45f2745ab085375588a6f6345b6848895a
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-04-27 16:12:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -33,24 +33,15 @@ function App() {
           <>
             <Header />
             <Routes>
-              <Route
-                path="/"
-                element={<Home />}></Route>
-              <Route
-                path="/signup"
-                element={<Signup />}></Route>
-              <Route
-                path="/signup-options"
-                element={<SignupOptions />}></Route>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
+              <Route path="/signup-options" element={<SignupOptions />}></Route>
               <Route
                 path="/user-options"
-                element={<SignupChooseUser />}></Route>
-              <Route
-                path="/signup-done"
-                element={<SignupDone />}></Route>
-              <Route
-                path="/Profile"
-                element={<Profile />}></Route>
+                element={<SignupChooseUser />}
+              ></Route>
+              <Route path="/signup-done" element={<SignupDone />}></Route>
+              <Route path="/Profile" element={<Profile />}></Route>
             </Routes>
           </>
         ) : (
@@ -59,40 +50,42 @@ function App() {
               <div style={{ backgroundColor: "#F5F7F9" }}>
                 <Navbar />
                 <Routes>
-                  <Route
-                    path="/"
-                    element={<SearchEmployee />}></Route>
-                  <Route
-                    path="/profile"
-                    element={<EmployerProfile />}></Route>
+                  <Route path="/" element={<SearchEmployee />}></Route>
+                  <Route path="/profile" element={<EmployerProfile />}></Route>
                   <Route
                     path="/employer-approval"
-                    element={<Approval />}></Route>
-                  <Route
-                    path="/requests"
-                    element={<Requests />}></Route>
+                    element={<Approval />}
+                  ></Route>
+                  <Route path="/requests" element={<Requests />}></Route>
                   <Route
                     path="/approvalRequest-form"
-                    element={<RequestApproval />}></Route>
+                    element={<RequestApproval />}
+                  ></Route>
                   <Route
                     path="/ViewAssessment"
-                    element={<ViewAssessment />}></Route>
+                    element={<ViewAssessment />}
+                  ></Route>
 
                   <Route
                     path="/Assessment-form"
-                    element={<EmployeeAssessmentForm />}></Route>
+                    element={<EmployeeAssessmentForm />}
+                  ></Route>
                   <Route
                     path="/EmployeeAssessment"
-                    element={<EmployeeAssessment />}></Route>
+                    element={<EmployeeAssessment />}
+                  ></Route>
                   <Route
                     path="/employee-details"
-                    element={<EmployeeDetails />}></Route>
+                    element={<EmployeeDetails />}
+                  ></Route>
                   <Route
                     path="/onboarding-form"
-                    element={<OnboardingForm />}></Route>
+                    element={<OnboardingForm />}
+                  ></Route>
                   <Route
                     path="/offerletter-sent"
-                    element={<OfferLetterSent />}></Route>
+                    element={<OfferLetterSent />}
+                  ></Route>
                 </Routes>
               </div>
             ) : (
@@ -104,28 +97,31 @@ function App() {
                 <div style={{ backgroundColor: "#F5F7F9" }}>
                   <Navbar />
                   <Routes>
-                    <Route
-                      path="/"
-                      element={<Profile />}></Route>
+                    <Route path="/" element={<Profile />}></Route>
                     <Route
                       path="/employeeOfferLetter"
-                      element={<EmployeeOfferLetter />}></Route>
+                      element={<EmployeeOfferLetter />}
+                    ></Route>
                     {/* <Route path="/offerLetters" element={<Offers />}></Route> */}
                     <Route
                       path="/colleagues"
-                      element={<SearchEmployee />}></Route>
+                      element={<SearchEmployee />}
+                    ></Route>
                     <Route
                       path="/EmployeeAssessment"
-                      element={<EmployeeAssessment />}></Route>
+                      element={<EmployeeAssessment />}
+                    ></Route>
                     {/*<Route path="/employer-approval" element={<Approval />}></Route>
                    <Route path="/verification-request" element={<VerficationRequest />}></Route>
                   <Route path="/assessment" element={<Assessment />}></Route>*/}
                     <Route
                       path="/employee-details"
-                      element={<EmployeeDetails />}></Route>
+                      element={<EmployeeDetails />}
+                    ></Route>
                     <Route
                       path="/verification-request"
-                      element={<VerficationRequest />}></Route>
+                      element={<VerficationRequest />}
+                    ></Route>
                   </Routes>
                 </div>
               ) : (
