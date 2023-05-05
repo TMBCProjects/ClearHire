bc30c92cea464e030f1a81e9ee86cd910fa1c032
giridhar2000 <48080862+giridhar2000@users.noreply.github.com>
2023-05-05 19:30:21 +0530

    Update Assessment.jsx

--- src/pages/Employee/Assessment/Assessment.jsx
+++ src/pages/Employee/Assessment/Assessment.jsx
@@ -1,20 +1,19 @@
-
 import React, { useEffect } from 'react'
 import "./AssessmentForm.css";
 import arrow from "../../../images/arrow-dropup.svg";
 import { useNavigate } from "react-router-dom";
-import { Button } from 'antd';
+import { Button, Empty } from 'antd';
 import { useState } from 'react';
 import check_1 from "../../../images/Check-1.svg";
-import { readAssessment } from '../../../DataBase/Employer/employer';
+import { readAssessment } from '../../../DataBase/Employee/employee';
+import { submitAssessment } from '../../../DataBase/Employee/employee';
 
 export default function EmployeeAssesmentForm() {
-  const ansType = ["Short Answer", "MCQ", "Select"]
   const [questions, setQuestions] = useState([])
-  const [qType, setQType] = useState([])
-  let values = [];
-  let type = [];
-  var questionDetails = []
+  const [answers, setAnswers] = useState([])
+  //const [checkedItems ,setCheckedItems] = useState([])
+  const checkedItems = []
+  const ques_no = []
   const navigate = useNavigate();
   const handleBack = () => {
     navigate("/");
@@ -32,29 +31,46 @@ export default function EmployeeAssesmentForm() {
       }
     };
     fetchAssessment();
+    console.log(questions)
   }, [])
 
-  const handleQuesChange = (e, i) => {
-    for (let j = 0; j <= qCount - 1; j++) {
-      values[j] = document.getElementById(j).value;
-      type[j] = document.getElementById("type" + j).value;
-      questionDetails.push({ question_no: j + 1, question: values[j], type: type[j] })
-    }
-    setQType(questionDetails)
-    console.log(questionDetails)
-    setQuestions(questions => ({
-      ...questions,
-      questionDetails
-    }))
+  const handleChange = (event, question_no, i) => {
+    const { value, checked } = event.target;
+    setAnswers(prevAns => {
+      const updatedAns = [...prevAns];
+      let selectedValues = []
+      if (updatedAns[i] === undefined) {
+        selectedValues = []
+      }
+      else {
+        selectedValues = updatedAns[i].answer.slice();
+      }
+      if (checked) {
+        selectedValues.push(value);
+      } else {
+        const index = selectedValues.indexOf(value);
+        if (index !== -1) {
+          selectedValues.splice(index, 1);
+        }
+      }
+      updatedAns[i] = { ...updatedAns[i], question_no: question_no, answer: selectedValues };
+      return updatedAns;
+    });
+  };
 
+  const handleInputChange = (e, question_no, i) => {
+    setAnswers(prevAns => {
+      const updatedAns = [...prevAns];
+      updatedAns[i] = { ...updatedAns[i], question_no: question_no, answer: e.target.value };
+      return updatedAns;
+    });
   }
 
-  const handleChange = () => {
-
-  }
-
-  const submitQues = () => {
-    console.log(questions)
+  const submitQues = async (i) => {
+    await submitAssessment(answers, i)
+      .then(() => {
+        window.location.href = "/"
+      })
   }
 
 
@@ -70,39 +86,65 @@ export default function EmployeeAssesmentForm() {
       <div className="assess-form">
         <div className="form">
           <form action="" className='assessform-1'>
+            {questions.length === 0 && <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No Questions"/>}
             {questions.map((e, i) => (
               <>
                 <div className="form-1">
                   <h1>{e.title}</h1><hr />
                   <h2>{e.description} </h2>
-                </div><br />
-                <div key={i}>
-                  {e.questionsList.map((value,i)=>{
-                    <div className="form-2" style={{ display: "flex" }}>
-                    <label htmlFor="">Q{i + 1}</label>{console.log(value,"sdaas")}
-                    <h3 className='question'>{value[i]}</h3>
-                  </div>
-                  })}
-                  
-
-                  {e.questionsList[i+1].type === "MCQ" &&
-                    <div style={{ marginBottom: "4vh" }} id={"options" + i}>
-                      <div style={{ display: "flex", flexDirection: 'row' }}>
-                        <input className='radio' type={"radio"}></input>
-                        <label for="html">{e.questionsList[i+1].option[i+1]}</label>
-                      </div>
-                    </div>
-                  }
-                  <hr />
                 </div>
+                <div><br />
+                  {e.questionsList.map((value, i) => (
+                    <div>
+                      <div className="form-2" style={{ display: "flex" }}>
+                        <label htmlFor="">Q{value.question_no}</label>
+                        <p className='question'>{value.question}</p>
+                      </div>
+                      {(e.questionsList[i].type === "MCQ" ||
+                        e.questionsList[i].type === "Select") &&
+                        <div>
+                          {value.option.map((val) => (
+                            <div style={{ marginBottom: "4vh", marginTop: "2vh" }} id={"options" + i}>
+                              <div style={{ display: "flex", flexDirection: 'row' }}>
+                                <input
+                                  onChange={e.questionsList[i].type === "Select" ?
+                                    (e) => handleChange(e, value.question_no, i)
+                                    :
+                                    (e) => handleInputChange(e, value.question_no, i)}
+                                  className='radio'
+                                  name={'option' + value.question_no}
+                                  type={e.questionsList[i].type === "Select" ? "checkbox" : "radio"}
+                                  value={val}></input>
+                                <label for="html">{val}</label>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      }
+
+                      {e.questionsList[i].type === "Short Answer" &&
+                        <div>
+                          <div style={{ marginBottom: "4vh" }} id={"options" + i}>
+                            <div style={{ display: "flex", flexDirection: 'row' }}>
+                              <input onChange={(e) => handleInputChange(e, value.question_no, i)} className='f-2' type={"text"} placeholder="Enter your Answer"></input>
+                            </div>
+                          </div>
+                        </div>
+                      }
+                    </div>
+                  ))}
+                  <br />
+                </div>
+                <Button onClick={()=>submitQues(e.id)} >
+                  <img className='checkimg' src={check_1} alt="" width={20} />&nbsp;
+                  Submit</Button>
               </>
+
             ))}
 
           </form>
 
-          <Button onClick={submitQues} >
-            <img className='checkimg' src={check_1} alt="" width={20} />&nbsp;
-            Submit</Button>
+
         </div>
 
       </div>
