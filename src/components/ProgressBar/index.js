97c55e40dba0b3244edbec038d9b0ad85bcbbde0
srivz <srivathsananim@gmail.com>
2023-05-18 18:30:16 +0530

    modified:   src/components/ProgressBar/index.js 	modified:   src/pages/Employee/Profile/Profile.jsx

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -3,26 +3,26 @@ import "./styles.css";
 import { RadialProgress } from 'react-radial-progress-indicator';
 const index = ({ value,color }) => {
     return (
-        <div className="progressLow">
-
-            <RadialProgress
-                backgroundColour="#dff0d8"
-                backgroundTransparent
-                duration={500}
-                fontRatio={6}
-                ringBgColour="#E3E3E3"
-                ringFgColour={color ? color :"#00823B"}
-                ringIntermediateColour="#59648D"
-                ringThickness={.2}
-                segmented
-                showIntermediateProgress
-                startStep={0}
-                step={value / 10}
-                steps={10}
-                text={function text( proportion) { return "".concat(Math.floor(value), "%") }}
-            />
-        </div >
-
+      <div className="progressLow">
+        <RadialProgress
+          backgroundColour="#dff0d8"
+          backgroundTransparent
+          duration={500}
+          fontRatio={6}
+          ringBgColour="#E3E3E3"
+          ringFgColour={color ? color : "#00823B"}
+          ringIntermediateColour="#59648D"
+          ringThickness={0.2}
+          segmented
+          showIntermediateProgress
+          startStep={0}
+          step={value / 10}
+          steps={10}
+          text={function text(proportion) {
+            return "".concat(Math.floor(value), "%");
+          }}
+        />
+      </div>
     );
 };
 
