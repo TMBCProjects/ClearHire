35a40a20206127ff18e2d859a6b1d3ffbb1a3bbf
srivz <srivathsananim@gmail.com>
2023-05-22 14:09:37 +0530

    modified:   cors.json 	modified:   src/firebase-config.js 	modified:   src/utils/Email.js

--- src/utils/Email.js
+++ src/utils/Email.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function sendEmail(email, subject, output) {
   try {
     const res = await axios.post(
-      "https://us-central1-teambo-c231b.cloudfunctions.net/sendMail",
+      "https://us-central1-clearhire-28c23.cloudfunctions.net/sendMail",
       {
         subject,
         email,
@@ -11,8 +11,10 @@ export default async function sendEmail(email, subject, output) {
       }
     );
     if (res.status === 200) {
+      alert("Sent Mail!");
       return true;
     } else {
+      alert("Mail is not sent!");
       return false;
     }
   } catch (err) {
