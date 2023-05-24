cdd89e6b4d71d090d8259cfc057cf56b0f2d9609
srivz <srivathsananim@gmail.com>
2023-05-24 11:49:00 +0530

    modified:   src/components/Login/Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,7 +8,7 @@ import LoginUser, {
 } from "../../DataBase/Login/login";
 import "./Login.css";
 import { Link } from "react-router-dom";
-import Loader from '../Loader'
+import Loader from "../Loader";
 import { GoogleOutlined } from "@ant-design/icons";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebase-config";
@@ -19,7 +19,7 @@ const initialValues = {
 const Login = () => {
   const [values, setValues] = useState(initialValues);
   const [messageApi, contextHolder] = message.useMessage();
-  const [loading,setLoading]=useState(false)
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   // const success = () => {
@@ -92,9 +92,9 @@ const Login = () => {
     });
   };
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      setLoading(true)
+      setLoading(true);
       let user = await LoginUser(values.email, values.password);
       if (user.photoURL === "Employer") {
         sessionStorage.setItem("LoggedIn", "Employer");
@@ -118,105 +118,118 @@ const Login = () => {
         navigate("/");
       }
     } catch (err) {
-      setLoading(false)
+      setLoading(false);
       error(err.message);
     }
   };
   return (
     <>
-    
-    {
-      loading &&
-      <Loader text={"Logging in..."} color={"#000"} textColor={"#fff"}/>
-    }
-    <div className="container-fluid" id="login">
-    
-      {contextHolder}
-      <div className="container" >
-        <div className="row d-flex justify-content-center align-items-center">
-          <div className="col-lg-5 col-12">
-            <h1 className="heading  text-white fw-bold">
-              Eprehenderit in{" "}
-              <span className="text-color-green">voluptate velit</span> esse
-              cillum auptate
-            </h1>
-            <p className="text-light mt-3">
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-              aut fugit, sed quia consequuntur magni dolores eos qui ratione
-              voluptatem sequ.
-            </p>
-          </div>
-          <div className="col-lg-6 col-12">
-            <div className="login-form">
-              <p className="login-heading">Login to your account</p>
+      {loading && (
+        <Loader
+          text={"Logging in..."}
+          color={"#000"}
+          textColor={"#fff"}
+        />
+      )}
+      <div
+        className="container-fluid"
+        id="login">
+        {contextHolder}
+        <div className="container">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-lg-5 col-12">
+              <h1 className="heading  text-white fw-bold">
+                Eprehenderit in{" "}
+                <span className="text-color-green">voluptate velit</span> esse
+                cillum auptate
+              </h1>
+              <p className="text-light mt-3">
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequ.
+              </p>
+            </div>
+            <div className="col-lg-6 col-12">
+              <div className="login-form">
+                <p className="login-heading">Login to your account</p>
 
-              <form className="w-75 mx-auto">
-                <div className="mb-3">
-                  <input
-                    type="email"
-                    name="email"
-                    className="form-control"
-                    placeholder="Email address"
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className="mb-3">
-                  <input
-                    type="password"
-                    name="password"
-                    className="form-control"
-                    placeholder="Password"
-                    onChange={handleInputChange}
-                  />
-                </div>
-
-                <div className="mb-3">
-                  <p>
-                    Forgot Password?{" "}
-                      <Link to={"/forgot-password"} style={{
-                        textDecoration: "none"
-                      }}><span className="text-color-green fw-bold">Click here</span></Link>
-                  </p>
-                </div>
-               
-                <div className="mb-3">
-               
-                <button
-                type="submit"
-                onClick={handleSubmit}
-                className="btn login-btn"
-                >
-               
-                Login
-                  </button>
-                </div>
-                <div className="mb-3">
-                  <p>
-                    Don't have an account?{" "}
-                    <Link to="/signup-options" className="text-decoration-none">
-                      <span className="text-color-green fw-bold">Sign up</span>
-                    </Link>
-                  </p>
-                  </div><div className="divider-line">
-                    <hr />
-                    <div className="divider-text">Or</div>
-                    <hr />
+                <form className="w-75 mx-auto">
+                  <div className="mb-3">
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Email address"
+                      onChange={handleInputChange}
+                    />
                   </div>
-                  <div className="buttons">
-                    <button onClick={handleLoginWithGoogle}>
-                      <GoogleOutlined style={{ fontSize: "30px", marginRight: "10px" }} />
-                      CONTINUE WITH GOOGLE
+                  <div className="mb-3">
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      placeholder="Password"
+                      onChange={handleInputChange}
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <p>
+                      Forgot Password?{" "}
+                      <Link
+                        to={"/forgot-password"}
+                        style={{
+                          textDecoration: "none",
+                        }}>
+                        <span className="text-color-green fw-bold">
+                          Click here
+                        </span>
+                      </Link>
+                    </p>
+                  </div>
+
+                  <div className="mb-3">
+                    <button
+                      type="submit"
+                      onClick={handleSubmit}
+                      className="btn login-btn">
+                      Login
                     </button>
                   </div>
-                </form>
+                  <div className="mb-3">
+                    <p>
+                      Don't have an account?{" "}
+                      <Link
+                        to="/signup-options"
+                        className="text-decoration-none">
+                        <span className="text-color-green fw-bold">
+                          Sign up
+                        </span>
+                      </Link>
+                    </p>
 
+                  </div>
+                </form>
+                <div className="divider-line">
+                  <hr />
+                  <div className="divider-text">Or</div>
+                  <hr />
+                </div>
+                <div className="buttons">
+                  <button onClick={handleLoginWithGoogle}>
+                    <GoogleOutlined
+                      style={{ fontSize: "30px", marginRight: "10px" }}
+                    />
+                    CONTINUE WITH GOOGLE
+                  </button>
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-    
   );
 };
 
