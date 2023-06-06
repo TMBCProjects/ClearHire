7e15e5c87979d2da92af30cbe2e43e755ed9d499
srivz <srivathsananim@gmail.com>
2023-06-06 22:42:51 +0530

    modified:   src/pages/SearchEmployee/SearchEmployee.jsx

--- src/pages/SearchEmployee/SearchEmployee.jsx
+++ src/pages/SearchEmployee/SearchEmployee.jsx
@@ -27,10 +27,6 @@ export default function SearchEmployee() {
     location: "",
     designation: "",
   });
-  const [filters2, setFilters2] = useState({
-    typeOfEmployment: "",
-    designation: "",
-  });
   useEffect(() => {
     const fetchCollegueDetails = async () => {
       try {
@@ -73,135 +69,76 @@ export default function SearchEmployee() {
       [field]: event.target ? event.target.value : event,
     }));
   };
-  const handleInput2Change = (event, field) => {
-    setFilters2((prevFilters) => ({
-      ...prevFilters,
-      [field]: event.target ? event.target.value : event,
-    }));
-  };
 
   return (
     <div className="employer-home">
-      {
-        user === "Employer" && (
-          <div
-            className="search-inputs mobile-filters"
-            style={{ position: "absolute" }}>
-            <div className="input-box1 input-box">
-              <img
-                src={search1}
-                alt="Search"
-              />
-              <input
-                type="text"
-                name="designation"
-                onChange={(e) => {
-                  handleInputChange(e, e.target.name);
-                }}
-                className="box-input"
-                placeholder="Job Title / Designation"
-              />
-            </div>
-            <div className="input-box2 input-box">
-              <img
-                src={location}
-                alt="Search"
-              />
-              <Select
-                onChange={(e) => {
-                  handleInputChange(e, "location");
-                }}
-                className="box-select"
-                placeholder="Location"
-                options={[{ value: "", label: "" }].concat(
-                  userDatas.data.companyLocations.map((option) => ({
-                    value: option,
-                    label: option,
-                  }))
-                )}
-              />
-            </div>
-            <div className="input-box3 input-box">
-              <img
-                src={job}
-                alt="Search"
-              />
-              <Select
-                type="text"
-                onChange={(e) => {
-                  handleInputChange(e, "typeOfEmployment");
-                }}
-                className="box-select"
-                placeholder="Type Of Employment"
-                options={[
-                  { value: "Any", label: "Any" },
-                  { value: "Permanent Full-Time", label: "Permanent Full-Time" },
-                  { value: "Part-Time", label: "Part-Time" },
-                  { value: "Casual/Vacation", label: "Casual/Vacation" },
-                  { value: "Contract", label: "Contract" },
-                  { value: "Internship/Trainee", label: "Internship/Trainee" },
-                ]}
-              />
-            </div>
-            <div className="input-box4 input-box ">
-              <img
-                src={salary}
-                alt="Search"
-              />
-              <input
-                type="text"
-                className="box-input no-border"
-                name="salary"
-                placeholder="Salary"
-                onChange={(e) => handleInputChange(e, e.target.name)}
-              />
-            </div>
-          </div>
-        )}
-      {user === "Employee" && (
-        <div
-          className="search-inputs mobile-filters"
-          style={{ position: "absolute" }}>
-          <div className="input-box1 input-box">
-            <img
-              src={search1}
-              alt="Search"
-            />
-            <input
-              type="text"
-              name="designation2"
-              onChange={(e) => {
-                handleInput2Change(e, e.target.name);
-              }}
-              className="box-input"
-              placeholder="Job Title / Designation"
-            />
-          </div>
-          <div className="input-box3 input-box">
-            <img
-              src={job}
-              alt="Search"
-            />
+      <div
+        className="search-inputs mobile-filters"
+      >
+        <div className="input-box1 input-box">
+          <img src={search1} alt="Search" />
+          <input
+            type="text"
+            name="designation"
+            onChange={(e) => handleInputChange(e, e.target.name)}
+            className="box-input"
+            placeholder="Job Title / Designation"
+          />
+        </div>
+        {user === "Employer" ? (
+          <div className="input-box2 input-box">
+            <img src={location} alt="Search" />
             <Select
-              type="text"
               onChange={(e) => {
-                handleInput2Change(e, "typeOfEmployment2");
+                handleInputChange(e, "location");
               }}
               className="box-select"
-              defaultValue={"Any"}
-              placeholder="Type Of Employment"
-              options={[
-                { value: "Any", label: "Any" },
-                { value: "Permanent Full-Time", label: "Permanent Full-Time" },
-                { value: "Part-Time", label: "Part-Time" },
-                { value: "Casual/Vacation", label: "Casual/Vacation" },
-                { value: "Contract", label: "Contract" },
-                { value: "Internship/Trainee", label: "Internship/Trainee" },
-              ]}
+              placeholder="Location"
+              options={[{ value: "", label: "" }].concat(
+                userDatas.data.companyLocations.map((option) => ({
+                  value: option,
+                  label: option,
+                }))
+              )}
             />
           </div>
+        ) : (
+          ""
+        )}
+        <div className="input-box3 input-box">
+          <img src={job} alt="Search" />
+          <Select
+            type="text"
+            onChange={(e) => {
+              handleInputChange(e, "typeOfEmployment");
+            }}
+            className="box-select"
+            placeholder="Type Of Employment"
+            options={[
+              { value: "", label: "" },
+              { value: "Permanent Full-Time", label: "Permanent Full-Time" },
+              { value: "Part-Time", label: "Part-Time" },
+              { value: "Casual/Vacation", label: "Casual/Vacation" },
+              { value: "Contract", label: "Contract" },
+              { value: "Internship/Trainee", label: "Internship/Trainee" },
+            ]}
+          />
         </div>
-      )}
+        {user === "Employer" ? (
+          <div className="input-box4 input-box ">
+            <img src={salary} alt="Search" />
+            <input
+              type="text"
+              className="box-input no-border"
+              name="salary"
+              placeholder="Salary"
+              onChange={(e) => handleInputChange(e, e.target.name)}
+            />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
       <div className="search-results">
         <div className="result-employees">
           <div className="row1">
@@ -237,24 +174,20 @@ export default function SearchEmployee() {
               />
             )}
             {employeeList
-              ?.filter((item) => {
-                const { typeOfEmployment, designation, salary, location } = filters;
-                const { typeOfEmployment2, designation2 } = filters2;
-                if (user === "Employer") {
-                  return (
-                    (typeOfEmployment === "Any" || item.typeOfEmployment.toLowerCase() === typeOfEmployment.toLowerCase()) &&
-                    (designation === "" || item.designation.toLowerCase().includes(designation.toLowerCase())) &&
-                    (salary === "" || +item?.salary <= +salary) &&
-                    (location === "" || item.companyLocation.toLowerCase() === location.toLowerCase())
-                  );
-                } else if (user === "Employee") {
-                  return (
-                    (item?.companyLocation === userDatas.data?.companyLocation) &&
-                    (typeOfEmployment2 === "Any" || item.typeOfEmployment === typeOfEmployment2) &&
-                    (designation2 === "" || item.designation.toLowerCase().includes(designation2.toLowerCase()))
-                  );
-                }
-                return true;
+              .filter((item) => {
+                const { typeOfEmployment, designation, salary, location } =
+                  filters;
+                return (
+                  (typeOfEmployment === "" ||
+                    item.typeOfEmployment.toLowerCase() ===
+                    typeOfEmployment.toLowerCase()) &&
+                  (designation === "" ||
+                    item.designation.toLowerCase().includes(designation)) &&
+                  (salary === "" || +item?.salary <= +salary) &&
+                  (location === "" ||
+                    item.companyLocation.toLowerCase() ===
+                    location.toLowerCase())
+                );
               })
               .map((info) => {
                 return (
