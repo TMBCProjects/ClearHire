4a65ee474555642c6b24c8ee23366a7e5f18113e
giridhar <giribala2001@gmail.com>
2023-06-07 09:07:21 +0530

    time fiexed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,23 +3,9 @@ import Login from "../../components/Login/Login";
 import About from "../../components/About/About";
 import Banner from "../../components/Bannner/Banner";
 import Footer from "../../components/Footer/Footer";
-import axios from "axios";
 
 const Home = () => {
 
-    const makeAPICall = async () => {
-        try {
-            await fetch("https://worldtimeapi.org/api/ip")
-                .then(response => response.json())
-                .then(data => console.log(data.datetime));
-        }
-        catch (e) {
-            console.log(e)
-        }
-    }
-    useEffect(() => {
-        makeAPICall();
-    }, [])
     return (
         <div>
             <Banner />
