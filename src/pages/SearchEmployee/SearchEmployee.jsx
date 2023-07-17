e7bb4bb45bfe03e1d992c271352d608ae2eebde4
giridhar <giribala2001@gmail.com>
2023-07-17 20:42:21 +0530

    filters ui complete

--- src/pages/SearchEmployee/SearchEmployee.jsx
+++ src/pages/SearchEmployee/SearchEmployee.jsx
@@ -4,17 +4,21 @@ import search1 from "../../assets/images/search.svg";
 import location from "../../assets/images/location.svg";
 import job from "../../assets/images/job.svg";
 import salary from "../../assets/images/salary.svg";
-import { Select, Empty, Drawer, Space, Button } from "antd";
-import AssesmentCard from "../../components/Cards/AssesmentCard";
+import { Select, Drawer, Space, Button, Tag } from "antd";
 import { readEmployees } from "../../DataBase/Employer/employer";
 import { readColleagues } from "../../DataBase/Employee/employee";
 import suitcase from "../../assets/images/suitcase.png"
+import clock from "../../assets/images/clock.png"
+import star from "../../assets/images/star.png"
+import { Slider } from 'antd';
 
 export default function SearchEmployee() {
   const userDatas = JSON.parse(sessionStorage.getItem("userData"));
   const user = sessionStorage.getItem("LoggedIn");
   const [employeeList, setEmployeeList] = useState([]);
-  const [open, setOpen] = useState(false)
+  const [openJT, setOpenJT] = useState(false)
+  const [openE, setOpenE] = useState(false)
+  const [openS, setOpenS] = useState(false)
   const [filters, setFilters] = useState({
     typeOfEmployment: "",
     salary: "",
@@ -23,6 +27,10 @@ export default function SearchEmployee() {
   });
   const [isMobile, setIsMobile] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const { CheckableTag } = Tag;
+  const JTData = ['Senior Designer', 'Senior Graphic Designer', 'Human Resource', 'Video Editor', 'UI UX Designer'
+    , 'Developer', 'Client Servicing'];
 
   const showMenu = () => {
     setMenuOpen(true);
@@ -30,6 +38,14 @@ export default function SearchEmployee() {
 
   const onMenuClose = () => {
     setMenuOpen(false);
+  };
+
+  const handleTagChange = (tag, checked) => {
+    const nextSelectedTags = checked
+      ? [...selectedTags, tag]
+      : selectedTags.filter((t) => t !== tag);
+    console.log('You are interested in: ', nextSelectedTags);
+    setSelectedTags(nextSelectedTags);
   };
 
   useEffect(() => {
@@ -180,85 +196,98 @@ export default function SearchEmployee() {
       <div className="employer-home">
         <div>
           <div className="filters">
-            <div className="col-sm-4 col-sm-offset-4" style={{width: "100%"}}>
+            <div className="col-sm-4 col-sm-offset-4" style={{ width: "100%" }}>
               <div className="panel-group">
                 <div className="panel panel-default">
                   <div className="panel-heading">
                     <h4 className="panel-title">
-                      <div className="filtersdiv" onClick={() => setOpen(!open)}>
-                        <div>
-                        <img src={suitcase} alt="job" />
-                        <span>Job Title</span>
+                      <div className="filtersdiv">
+                        <div className="eachfilter" onClick={() => setOpenJT(!openJT)}>
+                          <div>
+                            <img src={suitcase} alt="job" className="filtersimg" />
+                            <span>Job Title</span>
+                          </div>
+                          <span>+</span>
                         </div>
-                        <span>+</span>
+                        <div className={openJT ? "panel-collapse" : "panel-collapse panel-close"}>
+                          {JTData.map((tag) => (
+                            <CheckableTag
+                              key={tag}
+                              checked={selectedTags.includes(tag)}
+                              onChange={(checked) => handleTagChange(tag, checked)}
+                            >
+                              <div>{tag}</div>
+                            </CheckableTag>
+                          ))}
+                        </div>
                       </div>
                     </h4>
-                  </div>
-                  <div className={open ? "panel-collapse" : "panel-collapse panel-close"}>
-                    <ul className="list-group">
-                      <li className="list-group-item">One</li>
-                      <li className="list-group-item">Two</li>
-                      <li className="list-group-item">Three</li>
-                    </ul>
                   </div>
                 </div>
               </div>
 
             </div>
 
-            <div className="col-sm-4 col-sm-offset-4" style={{width: "100%"}}>
+            <div className="col-sm-4 col-sm-offset-4" style={{ width: "100%" }}>
               <div className="panel-group">
                 <div className="panel panel-default">
                   <div className="panel-heading">
                     <h4 className="panel-title">
-                      <div className="filtersdiv" onClick={() => setOpen(!open)}>
-                        <div>
-                        <img src={suitcase} alt="job" />
-                        <span>Experience</span>
+                      <div className="filtersdiv">
+                        <div className="eachfilter" onClick={() => setOpenE(!openE)}>
+                          <div>
+                            <img src={clock} alt="job" className="filtersimg" />
+                            <span>Experience</span>
+                          </div>
+                          <span>+</span>
                         </div>
-                        <span>+</span>
+                        <div className={openE ? "panel-collapse" : "panel-collapse panel-close"}>
+                          <Slider
+                            range={{
+                              draggableTrack: true,
+                            }}
+                            defaultValue={[20, 50]}
+                          />
+                        </div>
                       </div>
                     </h4>
-                  </div>
-                  <div className={open ? "panel-collapse" : "panel-collapse panel-close"}>
-                    <ul className="list-group">
-                      <li className="list-group-item">One</li>
-                      <li className="list-group-item">Two</li>
-                      <li className="list-group-item">Three</li>
-                    </ul>
                   </div>
                 </div>
               </div>
 
             </div>
 
-            <div className="col-sm-4 col-sm-offset-4" style={{width: "100%"}}>
+            <div className="col-sm-4 col-sm-offset-4" style={{ width: "100%" }}>
               <div className="panel-group">
                 <div className="panel panel-default">
                   <div className="panel-heading">
                     <h4 className="panel-title">
-                      <div className="filtersdiv" onClick={() => setOpen(!open)}>
-                        <div>
-                        <img src={suitcase} alt="job" />
-                        <span>Score</span>
+                      <div className="filtersdiv">
+                        <div className="eachfilter" onClick={() => setOpenS(!openS)}>
+                          <div>
+                            <img src={star} alt="job" className="filtersimg" />
+                            <span>Score</span>
+                          </div>
+                          <span>+</span>
                         </div>
-                        <span>+</span>
+                        <div className={openS ? "panel-collapse" : "panel-collapse panel-close"}>
+                          <Slider
+                            range={{
+                              draggableTrack: true,
+                            }}
+                            defaultValue={[20, 50]}
+                          />
+                        </div>
                       </div>
                     </h4>
                   </div>
-                  <div className={open ? "panel-collapse" : "panel-collapse panel-close"}>
-                    <ul className="list-group">
-                      <li className="list-group-item">One</li>
-                      <li className="list-group-item">Two</li>
-                      <li className="list-group-item">Three</li>
-                    </ul>
-                  </div>
+
                 </div>
               </div>
 
             </div>
 
-            <Button type="primary" style={{width: "88%", margin: "3vh", height: "5vh", borderRadius: "1vh"}}>Apply Filter</Button>
+            <Button type="primary" style={{ width: "88%", margin: "3vh", height: "5vh", borderRadius: "1vh" }}>Apply Filter</Button>
           </div>
         </div>
       </div>
