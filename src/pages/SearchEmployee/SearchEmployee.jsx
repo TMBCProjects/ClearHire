04a403be5428d467d440deb828d7496b1c02ae55
giridhar <giribala2001@gmail.com>
2023-07-19 20:39:52 +0530

    ui complete

--- src/pages/SearchEmployee/SearchEmployee.jsx
+++ src/pages/SearchEmployee/SearchEmployee.jsx
@@ -4,14 +4,16 @@ import search1 from "../../assets/images/search.svg";
 import location from "../../assets/images/location.svg";
 import job from "../../assets/images/job.svg";
 import salary from "../../assets/images/salary.svg";
-import { Select, Drawer, Space, Button, Tag } from "antd";
+import { Select, Drawer, Space, Button, Tag, Card, Progress, Slider, Modal } from "antd";
 import { readEmployees } from "../../DataBase/Employer/employer";
 import { readColleagues } from "../../DataBase/Employee/employee";
 import suitcase from "../../assets/images/suitcase.png"
 import clock from "../../assets/images/clock.png"
 import star from "../../assets/images/star.png"
-import { Slider } from 'antd';
+import search from "../../assets/images/search.png"
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 
+const { Meta } = Card;
 export default function SearchEmployee() {
   const userDatas = JSON.parse(sessionStorage.getItem("userData"));
   const user = sessionStorage.getItem("LoggedIn");
@@ -31,7 +33,7 @@ export default function SearchEmployee() {
   const { CheckableTag } = Tag;
   const JTData = ['Senior Designer', 'Senior Graphic Designer', 'Human Resource', 'Video Editor', 'UI UX Designer'
     , 'Developer', 'Client Servicing'];
-
+  const formatter = (value) => `${value}%`;
   const showMenu = () => {
     setMenuOpen(true);
   };
@@ -107,92 +109,7 @@ export default function SearchEmployee() {
 
   return (
     <>
-      <Drawer
-        title="Clearhire"
-        placement={"left"}
-        onClose={onMenuClose}
-        open={menuOpen}
-      >
-        <div>
-          <div className="input-box1 input-box mb-3">
-            <img
-              src={search1}
-              alt="Search"
-              className={`${isMobile} && d-none`}
-            />
-            <input
-              type="text"
-              name="designation"
-              onChange={(e) => handleInputChange(e, e.target.name)}
-              className={`box-input ms-2 ${isMobile} && w-100 ms-0 p-2`}
-              placeholder="Job Title / Designation"
-            />
-          </div>
-          {user === "Employer" && (
-            <div className="input-box2 input-box mb-3">
-              <img
-                src={location}
-                alt="Search"
-                className={`${isMobile} && d-none`}
-              />
-              <Select
-                onChange={(e) => {
-                  handleInputChange(e, "location");
-                }}
-                className={`box-select ms-2 ${isMobile} && w-100`}
-                placeholder="Location"
-                options={[{ value: "", label: "" }].concat(
-                  userDatas?.data?.companyLocations.map((option) => ({
-                    value: option,
-                    label: option,
-                  }))
-                )}
-              />
-            </div>
-          )}
-          <div className="input-box3 input-box mb-3">
-            <img src={job} alt="Search" className={`${isMobile} && d-none`} />
-            <Select
-              type="text"
-              onChange={(e) => {
-                handleInputChange(e, "typeOfEmployment");
-              }}
-              className={`box-select ms-2 ${isMobile} && w-100`}
-              placeholder="Type Of Employment"
-              options={[
-                { value: "", label: "" },
-                { value: "Permanent Full-Time", label: "Permanent Full-Time" },
-                { value: "Part-Time", label: "Part-Time" },
-                { value: "Casual/Vacation", label: "Casual/Vacation" },
-                { value: "Contract", label: "Contract" },
-                { value: "Internship/Trainee", label: "Internship/Trainee" },
-              ]}
-            />
-          </div>
-          {user === "Employer" && (
-            <div className="input-box4 input-box ">
-              <img
-                src={salary}
-                alt="Search"
-                className={`${isMobile} && d-none`}
-              />
-              <input
-                type="text"
-                className={`box-input no-border ms-2 ${isMobile} && w-100 ms-0 p-2`}
-                name="salary"
-                placeholder="Salary"
-                onChange={(e) => handleInputChange(e, e.target.name)}
-              />
-            </div>
-          )}
-        </div>
-        <Space className="mt-3 d-flex justify-content-end">
-          <Button onClick={onMenuClose}>Cancel</Button>
-          <Button type="primary" onClick={onMenuClose}>
-            OK
-          </Button>
-        </Space>
-      </Drawer>
+
       <div className="employer-home">
         <div>
           <div className="filters">
@@ -207,7 +124,11 @@ export default function SearchEmployee() {
                             <img src={suitcase} alt="job" className="filtersimg" />
                             <span>Job Title</span>
                           </div>
-                          <span>+</span>
+                          {openJT ?
+                            <span>-</span>
+                            :
+                            <span>+</span>
+                          }
                         </div>
                         <div className={openJT ? "panel-collapse" : "panel-collapse panel-close"}>
                           {JTData.map((tag) => (
@@ -239,7 +160,11 @@ export default function SearchEmployee() {
                             <img src={clock} alt="job" className="filtersimg" />
                             <span>Experience</span>
                           </div>
-                          <span>+</span>
+                          {openE ?
+                            <span>-</span>
+                            :
+                            <span>+</span>
+                          }
                         </div>
                         <div className={openE ? "panel-collapse" : "panel-collapse panel-close"}>
                           <Slider
@@ -268,14 +193,17 @@ export default function SearchEmployee() {
                             <img src={star} alt="job" className="filtersimg" />
                             <span>Score</span>
                           </div>
-                          <span>+</span>
+                          {openS ?
+                            <span>-</span>
+                            :
+                            <span>+</span>
+                          }
                         </div>
                         <div className={openS ? "panel-collapse" : "panel-collapse panel-close"}>
                           <Slider
-                            range={{
-                              draggableTrack: true,
+                            tooltip={{
+                              formatter,
                             }}
-                            defaultValue={[20, 50]}
                           />
                         </div>
                       </div>
@@ -287,10 +215,54 @@ export default function SearchEmployee() {
 
             </div>
 
-            <Button type="primary" style={{ width: "88%", margin: "3vh", height: "5vh", borderRadius: "1vh" }}>Apply Filter</Button>
+            <Button style={{ width: "88%", margin: "3vh", height: "5vh", borderRadius: "1vh", backgroundColor: "#1f4aff", color: "white" }}>Apply Filter</Button>
+          </div>
+
+          <div className="search">
+            <input type="text" className="searchip" placeholder="Search" />
+            <button className="searchbtn"><img src={search} alt="search" style={{ width: "50%" }} /></button>
+          </div>
+
+          <div className="empCards">
+            <Card
+              style={{
+                width: 250,
+                borderRadius: "5vh"
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                  style={{
+                    padding: "5px",
+                    borderTopRightRadius: "2vh",
+                    borderTopLeftRadius: "2vh"
+                  }}
+                />
+              }
+              actions={[
+                <><SettingOutlined key="setting" /> Access</>,
+                <><EditOutlined key="edit" onClick={showModal}/> Rate</>,
+                <><EllipsisOutlined key="ellipsis" />More</>,
+              ]}
+            >
+              <Meta
+                title="Courtney"
+                description="Senior Designer"
+              />
+              <Progress percent={30} size="small" />
+            </Card>
+
+
           </div>
         </div>
       </div>
+
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </>
   );
 }
